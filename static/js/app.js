b853b020e3a977d2166555ca030e539c6f679829
Karika Shah <karikashah@yahoo.com>
2019-03-07 17:55:33 -0600

    Added the data from data.js in the table. & updated the background image.

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,16 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+tableData.forEach(function(ufoSightings) {
+  console.log(ufoSightings);
+  var row = tbody.append("tr");
+  Object.entries(ufoSightings).forEach(function([key, value]) {
+    console.log(key, value);
+    row.append("td").text(value);
+  });
+});
+
+
+  
