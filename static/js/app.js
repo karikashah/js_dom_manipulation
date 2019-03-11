e569e6a9216b0418fcda282c491c411d13fcdb1a
Karika Shah <karikashah@yahoo.com>
2019-03-11 15:03:02 -0500

    Finished Level 1 of the assignment. i.e. 1. Displaying of the complete data. 2. Disply of only that data i.e. being searched based on the date/time parameter.

--- static/js/app.js
+++ static/js/app.js
@@ -1,15 +1,39 @@
-// from data.js
-var tableData = data;
-
+var tableData = data; // from data.js
 var tbody = d3.select("tbody");
+var submit = d3.select("#filter-btn"); // Select the submit button
 
-tableData.forEach(function(ufoSightings) {
-  console.log(ufoSightings);
-  var row = tbody.append("tr");
-  Object.entries(ufoSightings).forEach(function([key, value]) {
-    console.log(key, value);
-    row.append("td").text(value);
+// for the first time page loads, populate all the content of the data (the UFO sightings)
+populateTable(tableData);
+
+// Actual function to display the UFO sightings data in the table
+function populateTable(ufoData) {
+  ufoData.forEach(function(ufoSighting) {
+    // console.log(ufoSighting);
+    var row = tbody.append("tr");
+    Object.entries(ufoSighting).forEach(function([key, value]) {
+      // console.log(key, value);
+      row.append("td").text(value);
+    });
   });
+}
+
+// When submit event is triggered
+submit.on("click", function() {
+  // Prevent the page from refreshing
+  d3.event.preventDefault();
+
+  // Select the input element and get the raw HTML node
+  var inputElement = d3.select("#datetime");
+
+  // Get the value property of the input element
+  var inputValue = inputElement.property("value");
+  console.log("Input Value", inputValue);
+  var filteredData = tableData.filter(dt => dt.datetime === inputValue);
+  console.log("Filter data", filteredData);
+  tbody.selectAll("tr").remove();
+
+  // populate the table with the filtered data
+  populateTable(filteredData);
 });
 
 
